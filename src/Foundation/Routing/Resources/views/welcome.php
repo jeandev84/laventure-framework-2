bd58130ab9f31e5ceb8e187b9d417c656ceab3e3
jeandev84 <jeandev84@yahoo.com>
2022-04-09 14:17:39 +0300

    add project name to template default route

--- src/Foundation/Routing/Resources/views/welcome.php
+++ src/Foundation/Routing/Resources/views/welcome.php
@@ -12,7 +12,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="<?= route('home') ?>"><?= app_name() ?></a> <!-- fix {{ app_name() }} -->
+        <a class="navbar-brand" href="<?= route('home') ?>">Laventure</a> <!-- fix {{ app_name() }} -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
