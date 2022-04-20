dff7b9351a29a6c763080f12abb79713a6186836
jeandev84 <jeandev84@yahoo.com>
2022-04-20 19:37:24 +0300

    hf

--- src/Foundation/Routing/Resources/views/welcome.php
+++ src/Foundation/Routing/Resources/views/welcome.php
@@ -5,16 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome to Laventure framework</title>
+    <title>Welcome to {{ app_name() }}</title>
     <link rel="stylesheet" href="{{ asset('/assets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/assets/css/app.css') }}">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('home') }}">
-            {{ app_name() }}
-        </a>
+        <a class="navbar-brand" href="{{ route('home') }}">{{ app_name() }}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
